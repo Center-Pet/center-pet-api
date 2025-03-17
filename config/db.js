cc006cdcaca983614fbb3b090b54a28d34091fea
efernades <enzo.dantas@hotmail.com>
2025-03-17 18:19:17 -0300

    0.0.2 conexão com banco e estrutura de arquivo

--- config/db.js
+++ config/db.js
@@ -6,7 +6,6 @@ console.log("Tentando conectar ao MongoDB...");
 
 const connectDB = async () => {
     try {
-        console.log("URI usada:", process.env.MONGO_URI);
         await mongoose.connect(process.env.MONGO_URI); // Removidas opções obsoletas
         console.log("🔥 MongoDB Atlas conectado com sucesso!");
     } catch (error) {
--- models/ong.js
+++ models/ong.js
@@ -1,5 +1,40 @@
 const mongoose = require("mongoose");
 
 const ongSchema = new mongoose.Schema({
-    
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    phone: {
+        type: String,
+        required: true
+    },
+    cep: {
+        type: String,
+        required: true
+    },
+    address: {
+        type: String,
+        required: true
+    },
+    city: {
+        type: String,
+        required: true
+    },
+    uf: {
+        type: String,
+        required: true
+    },
+    cnpj: {
+        type: String,
+        required: true
+    },
+    bio: {
+        type: String,
+        required: true
+    }
 })
