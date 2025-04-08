d981f14a75af3c4793262fd9ec559dd0fdd01718
Murilo <160414669+Celegattodev@users.noreply.github.com>
2025-04-08 18:06:23 -0300

    0.0.6 Create adopter.js

---
+++ models/adopter.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+
+const adopterSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  phone: { type: String, required: true },
+  address: {
+    state: { type: String, required: true },
+    city: { type: String, required: true },
+  },
+  registerDate: { type: Date, default: Date.now },
+  completedForm: { type: Boolean, default: false}
+})
+
+module.exports = mongoose.model('Adopter', usuarioSchema)
