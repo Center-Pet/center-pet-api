e237010317e1944be3cd0cf222acbeffd7303186
Murilo <160414669+Celegattodev@users.noreply.github.com>
2025-04-08 17:46:59 -0300

    0.0.4 Update ong.js

--- models/ong.js
+++ models/ong.js
@@ -1,40 +1,22 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
 
 const ongSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true
-    },
-    phone: {
-        type: String,
-        required: true
-    },
-    cep: {
-        type: String,
-        required: true
-    },
-    address: {
-        type: String,
-        required: true
-    },
-    city: {
-        type: String,
-        required: true
-    },
-    uf: {
-        type: String,
-        required: true
-    },
-    cnpj: {
-        type: String,
-        required: true
-    },
-    bio: {
-        type: String,
-        required: true
-    }
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  pixKey: String,
+  phone: { type: String, required: true},
+  city: { type: String, required: true },
+  profileImg: { type: String, required: true },
+  socialMidia: {
+    instagram: String,
+    facebook: String,
+    site: String
+  },
+  verified: { type: Boolean, default: false },
+  registerDate: { type: Date, default: Date.now },
+  petsRegisters: { type: Number, default: 0 },
+  petsAdopted: { type: Number, default: 0 }
 })
+
+module.exports = mongoose.model('Ong', ongSchema)
