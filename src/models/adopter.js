7a0fed8f49c22e20cbcf6f74147022daf634a915
fernandes <fernandesdenzo223@gmail.com>
2025-05-06 09:26:35 -0300

    0.3.3 update adopter.js

--- src/models/adopter.js
+++ src/models/adopter.js
@@ -11,7 +11,7 @@ const adopterSchema = new mongoose.Schema({
 
   //Informações do formulário
   //Informações pessoais
-  age: { type: Number }, //Idade do adotante
+  birth: { type: Date}, //Data de nascimento do adotante
   phone: { type: String}, //Telefone do adotante
   profession: { type: String}, //Profissão do adotante
   cep: { type: String}, //CEP do adotante
