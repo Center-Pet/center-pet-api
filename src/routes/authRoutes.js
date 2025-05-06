b9b19e0382ec0feaaacf68255796403683ebe44f
Luís Gustavo <francaguto10@gmail.com>
2025-05-05 21:01:39 -0300

    0.3.2 Create Logout
    Rota para logout criada (tokenBlacklist)

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { login } = require('../controllers/authController');
+const { login, logout } = require('../controllers/authController');
 
 // Rota de login unificada para Adopter e Ong
 router.post('/login', login);
+
+// Rota de logout
+router.post('/logout', logout);
 
 module.exports = router;
