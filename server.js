6ca0e9dc004059c29653f0cc6b2526909de1f011
fernandes <fernandesdenzo223@gmail.com>
2025-06-04 22:04:24 -0300

    0.6.0 add .envs

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
-require("dotenv").config();
-const connectDB = require("./src/config/db");
+const dotenvFlow = require('dotenv-flow');
+dotenvFlow.config();
 
+console.log("NODE_ENV:", process.env.NODE_ENV);
+console.log("MONGO_URI:", process.env.MONGO_URI);
+
+const connectDB = require("./src/config/db");
 connectDB();
 
 const app = require("./app");
--- src/config/db.js
+++ src/config/db.js
@@ -1,21 +1,17 @@
 // filepath: /c:/Users/dti/CenterPet.API/config/db.js
 const mongoose = require("mongoose");
-require("dotenv").config();
 
 console.log("Tentando conectar ao MongoDB...");
+console.log("MONGO_URI:", process.env.MONGO_URI); // Adicione este log para depuração
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI); // Removidas opções obsoletas
+        await mongoose.connect(process.env.MONGO_URI);
         console.log("🔥 MongoDB Atlas conectado com sucesso!");
     } catch (error) {
         console.error("❌ Erro ao conectar ao MongoDB Atlas:", error);
         process.exit(1);
     }
 };
-
-if (require.main === module) {
-    connectDB();
-}
 
 module.exports = connectDB;
